1fb8f449c9fc3a82a2ee6648071371d3208c0a9b
Kiho <kihoc22@gmail.com>
2017-11-08 21:37:28 -0800

    Pass inc/dec step from textbox

--- examples/svelte/counter/src/actions.ts
+++ examples/svelte/counter/src/actions.ts
@@ -1,4 +1,6 @@
 export const actions = store => ({
     increment: state => ({ count: state.count + 1 }),
-    decrement: state => ({ count: state.count - 1 })
+    decrement: state => ({ count: state.count - 1 }),
+    incrementOf: (state, step) => ({ count: state.count + step }),
+    decrementOf: (state, step) => ({ count: state.count - step }),
 });
