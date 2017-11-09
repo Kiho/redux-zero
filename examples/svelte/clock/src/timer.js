7aac7f293ba308defdf6d1a7dd91560510f92389
Kiho <kihoc22@gmail.com>
2017-11-08 22:25:10 -0800

    Refactor timer, fix date compare

--- examples/svelte/clock/src/timer.js
+++ examples/svelte/clock/src/timer.js
@@ -1,10 +1,9 @@
 import store from "./store";
 
 function create() {
-    store.setState({ time: new Date() });
     return setInterval(() => {
         store.setState({ time: new Date() });
     }, 1000 );
 }
 
-export { store, create }
+export { create }
