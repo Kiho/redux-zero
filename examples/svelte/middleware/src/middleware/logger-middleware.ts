98d8964fa5ded8771b158be6f68719c201218cac
Kiho <kihoc22@gmail.com>
2017-11-19 12:55:45 -0800

    Simplify getting Action Keys

--- examples/svelte/middleware/src/middleware/logger-middleware.ts
+++ examples/svelte/middleware/src/middleware/logger-middleware.ts
@@ -1,6 +1,6 @@
-const middleware = store => action => {
-  console.log(store.getState()) // okay, we can console.log state on every action execution
-  console.log(action) // some function. which function?
+const middleware = store => (action, name) => {
+  console.log('states', store.getState()) // okay, we can console.log state on every action execution
+  console.log('action', name) // some function. which function?
   return action
   // return highOrderFunction(action) // add some high order fn?
 }
