d7fe206a7089f10a0b57bee311bd638fa697fb60
Kiho <kihoc22@gmail.com>
2017-11-24 11:56:19 -0800

    Compare action signature in middleware

--- examples/svelte/middleware/src/middleware/promise-middleware.ts
+++ examples/svelte/middleware/src/middleware/promise-middleware.ts
@@ -2,15 +2,19 @@ import { actions } from "../asyncActions";
 import * as api from '../api';
 
 function getActionKeys(actions) {
-    return Object.keys(typeof actions === "function" ? actions() : actions);
+    return Object.entries(typeof actions === "function" ? actions() : actions);
 }
-const actionKeys = getActionKeys(actions);
+const actionKeys = getActionKeys(actions).map(x => ({ key: x[0], fn: String(x[1]) }));
 
 const middleware = store => (action, name) => {
-    const meth = actionKeys.find(x => x === name);
+    let actionToFn = String(action);
+    const meth = actionKeys.find(x => {
+        // console.log('actionKeys.find', actionToFn == x.fn, name, actionToFn);
+        return actionToFn === x.fn && name === x.key;
+    });
     if (meth) {
         store.setState({ loading: true });
-        return (state, path, body) => api[meth](path, body)
+        return (state, path, body) => api[meth.key](path, body)
                 .then(data => ({ [path]: data, loading: false }))
                 .catch(error => ({ error, loading: false }))
     }
