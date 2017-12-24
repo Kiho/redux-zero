1a8bcdc4d7f01574aface229a8539d2b7f5d83ee
Kiho <kihoc22@gmail.com>
2017-12-24 12:58:28 -0800

    Made replay work with store

--- examples/react/counter/src/dev-tools.js
+++ examples/react/counter/src/dev-tools.js
@@ -1,18 +1,54 @@
 let devtoolsMiddleware;
 let devTools;
+const nextActions = [];
 
 // if we have redux devtools
 if (window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+  function getOrAddAction(action, fn) {
+    let found = nextActions.find(x => {
+      console.log('actions.find', action.name, String(action));
+      return action.name === x.key;
+    });
+    if (!found) {
+      found = { key: action.name, fn };
+      nextActions.push(found);
+    }
+    return found;
+  }
+
+  function replay(store, message) {
+    const state = JSON.parse(message.state);
+    for(let key in state.actionsById) {
+      setTimeout(() => {
+        const thisAction = state.actionsById[key].action;
+        if (parseInt(key, 10) <= message.payload.id) {          
+          if (thisAction.type === 'initialState') {
+            store.setState(state.computedStates[0].state);
+          } else {
+            let found = nextActions.find(x => thisAction.type === x.key);
+            if (found) {
+              found.fn();
+              console.log('replay', thisAction.type, store.getState());
+            }
+          }
+        }
+      }, 100);
+    }
+  }
+
   function subscribe(store, middleware) {
     if (middleware.initialized) return;
-    //subscribe to devtools jump to action
+    //subscribe to devtools jump to action & toggle action
     devTools.subscribe(message => {
-      if (
-        message.type === 'DISPATCH' &&
-        (message.payload.type === 'JUMP_TO_ACTION' || message.payload.type === 'JUMP_TO_STATE')
-      ) {
-        store.setState(JSON.parse(message.state));
-      }
+      if (message.type === 'DISPATCH'){
+        if (
+          (message.payload.type === 'JUMP_TO_ACTION' || message.payload.type === 'JUMP_TO_STATE')
+        ) {
+          store.setState(JSON.parse(message.state));
+        } else if (message.payload.type === 'TOGGLE_ACTION') {
+          replay(store, message);
+        }
+      }      
     });
     middleware.initialized = true;
   }
@@ -21,6 +57,7 @@ if (window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__) {
   devtoolsMiddleware = store => next => action => {
     let result = next(action);
     subscribe(store, this);
+    getOrAddAction(action, () => next(action));
     devTools.send(action.name, store.getState());
     return result;
   };
