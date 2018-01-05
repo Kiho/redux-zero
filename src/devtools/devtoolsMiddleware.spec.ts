669f022202c144e6e0365366093b3a286dc9dca0
Kevin Chang <kchang6544@gmail.com>
2018-01-04 21:38:02 -0800

    added more tests

--- src/devtools/devtoolsMiddleware.spec.ts
+++ src/devtools/devtoolsMiddleware.spec.ts
@@ -7,25 +7,6 @@ import {
   getOrAddAction
 } from "./devtoolsMiddleware"
 
-const jumpToAction = {
-  type: "DISPATCH",
-  payload: { type: "JUMP_TO_ACTION" },
-  state: '{"count":3}'
-}
-const jumpToState = {
-  type: "DISPATCH",
-  payload: { type: "JUMP_TO_STATE" },
-  state: '{"count":4}'
-}
-const toggleAction = {
-  type: "DISPATCH",
-  payload: { type: "TOGGLE_ACTION", id: 1 },
-  state: `{"actionsById":{"0":{"action":{"type":"initialState"},"timestamp":1514964802390,"type":"PERFORM_ACTION"},
-    "1":{"action":{"type":"increment"},"timestamp":1514964812877,"type":"PERFORM_ACTION"},
-    "2":{"action":{"type":"decrement"},"timestamp":1514964817322,"type":"PERFORM_ACTION"}},
-    "computedStates":[{"state":{"count":1}},{"state":{"count":2}},{"state":{"count":1}}],
-    "currentStateIndex":2,"nextActionId":3,"skippedActionIds":[],"stagedActionIds":[0,1,2]}`
-}
 const increment = ({ count }) => ({ count: count + 1 })
 const decrement = ({ count }) => ({ count: count - 1 })
 const getActions = ({ getState }) => ({
@@ -54,62 +35,105 @@ function bindActions(actions, store) {
 
 describe("devtoolsMiddleware", () => {
   it("should jump to new action", () => {
+    const jumpToAction1 = {
+      type: "DISPATCH",
+      payload: { type: "JUMP_TO_ACTION" },
+      state: '{"count":2}'
+    }
+    
     const store = createStore({ count: 0 })
     const storeUpdate = update.bind(store)
 
-    storeUpdate(jumpToAction)
-    // console.log("jumpToAction", store.getState())
+    storeUpdate(jumpToAction1)
+    expect(store.getState()).toEqual({ count: 2 })
+
+    const jumpToAction2 = {
+      type: "DISPATCH",
+      payload: { type: "JUMP_TO_ACTION" },
+      state: '{"count":3}'
+    }
+
+    storeUpdate(jumpToAction2)
     expect(store.getState()).toEqual({ count: 3 })
   })
   it("should jump to new state", () => {
+
+    const jumpToState1 = {
+      type: "DISPATCH",
+      payload: { type: "JUMP_TO_STATE" },
+      state: '{"count":4}'
+    }
+
     const store = createStore({ count: 0 })
     const storeUpdate = update.bind(store)
 
-    storeUpdate(jumpToState)
-    // console.log("jumpToState", store.getState())
+    storeUpdate(jumpToState1)
     expect(store.getState()).toEqual({ count: 4 })
+
+    const jumpToState2 = {
+      type: "DISPATCH",
+      payload: { type: "JUMP_TO_STATE" },
+      state: '{"count":5}'
+    }
+
+      storeUpdate(jumpToState2)
+      expect(store.getState()).toEqual({ count: 5 })
   })
-  // it("should replay actions to current action", () => {
-  //   const store = createStore({ count: 0 })
-  //   const storeUpdate = update.bind(store)
-
-  //   storeUpdate(toggleAction)
-
-  //   jest.runAllTimers();
-  //   console.log("toggleAction", store.getState())
-  //   // Checks for correct amount of actions made
-  //   expect(setTimeout).toHaveBeenCalledTimes(2);
-  //   // Checks for correct current state
-  //   expect(store.getState()).toEqual({ count:1 })
-
-  // })
+ 
   it("should replay actions to current action", () => {
     const initialState = { count: 1 }
     const middlewares = applyMiddleware(devtoolsMiddleware)
 
     const store = createStore(initialState, middlewares)
     const actions = bindActions(getActions, store)
-    // devTools.send(action.name, store.getState())
+
     devTools.devTools = { send: () => {}, subscribe: () => {} }
 
-    // const store = createStore({ count: 0 }, applyMiddleware())
-    // const actions = bindActions(getActions, store)
     console.log("action name", Object.keys(actions))
-    // actions.increment()
-    // actions.decrement()
+
     Object.keys(actions).forEach(key => {
       getOrAddAction({ name: key }, actions[key])
     })
+
+    const toggleAction1 = {
+      type: "DISPATCH",
+      payload: { type: "TOGGLE_ACTION", id: 1 },
+      state: `{"actionsById":{"0":{"action":{"type":"initialState"},"timestamp":1514964802390,"type":"PERFORM_ACTION"},
+        "1":{"action":{"type":"increment"},"timestamp":1514964812877,"type":"PERFORM_ACTION"},
+        "2":{"action":{"type":"decrement"},"timestamp":1514964817322,"type":"PERFORM_ACTION"}},
+        "computedStates":[{"state":{"count":1}},{"state":{"count":2}},{"state":{"count":1}}],
+        "currentStateIndex":2,"nextActionId":3,"skippedActionIds":[],"stagedActionIds":[0,1,2]}`
+    }
+
     const storeUpdate = update.bind(store)
-    storeUpdate(toggleAction)
+    storeUpdate(toggleAction1)
 
     expect(store.getState().count).toBe(1)
 
     jest.runAllTimers()
     console.log("toggleAction", store.getState())
-    // Checks for correct amount of actions made
+
     expect(setTimeout).toHaveBeenCalledTimes(2)
-    // Checks for correct current state
     expect(store.getState()).toEqual({ count: 2 })
+
+    
+    const toggleAction2 = {
+      type: "DISPATCH",
+      payload: { type: "TOGGLE_ACTION", id: 2 },
+      state: `{"actionsById":{"0":{"action":{"type":"initialState"},"timestamp":1514964802390,"type":"PERFORM_ACTION"},
+        "1":{"action":{"type":"increment"},"timestamp":1514964812877,"type":"PERFORM_ACTION"},
+        "2":{"action":{"type":"decrement"},"timestamp":1514964817322,"type":"PERFORM_ACTION"}},
+        "computedStates":[{"state":{"count":1}},{"state":{"count":2}},{"state":{"count":1}}],
+        "currentStateIndex":2,"nextActionId":3,"skippedActionIds":[],"stagedActionIds":[0,1,2]}`
+    }
+
+    storeUpdate(toggleAction2)
+
+    expect(store.getState().count).toBe(2)
+    
+    jest.runAllTimers()
+    console.log("toggleAction", store.getState())
+    
+    expect(store.getState()).toEqual({ count: 1 })
   })
 })
