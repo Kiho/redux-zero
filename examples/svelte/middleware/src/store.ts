d63bacaa05cf3b5f0935a3305a15d46ec4284e76
Kiho <kihoc22@gmail.com>
2018-01-14 00:14:15 -0800

    Handle async actions in devtools middleware

--- examples/svelte/middleware/src/store.ts
+++ examples/svelte/middleware/src/store.ts
@@ -2,11 +2,13 @@ import createStore from "redux-zero";
 import { applyMiddleware } from "redux-zero/middleware";
 import logger  from './middleware/logger-middleware';
 import promise  from './middleware/promise-middleware';
+import { connect } from '../../../../devtools';
 
 const initialState = { count: 0, people: [] };
+const devtools = connect ? connect(initialState) : null;
 const middlewares = applyMiddleware(
-    logger,
     promise,
+    devtools,
 );
 const store = createStore(initialState, middlewares);
 
